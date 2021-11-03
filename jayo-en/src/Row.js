cc6a7265d13384ee3baefa5c991746253fc57b48
Naville30 <nahomhabteab2001@gmail.com>
2021-11-03 13:16:00 -0700

    Jayo Ent Frontend Demo

---
+++ jayo-en/src/Row.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import "./Row.css";
+import YouTube from "react-youtube";
+import moveTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original"
+
+function Row({title, fetchUrl, isLargeRow}){
+    const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
+    
+    //Snippet of code which runs based on specific condition
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+            
+        }   
+        fetchData(); 
+    }, [fetchUrl]);
+
+    const opts = {
+        height: "390",
+        width: "99%",
+        playerVars: {
+          autoplay: 0,
+        }
+      };
+
+    const handleClick = (movie) => {
+        if (trailerUrl){
+            setTrailerUrl('');
+        }   else {
+            moveTrailer(movie?.name || movie?.title || "")
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            }).catch((error) => console.log(error));
+        }
+    };
+
+    //console.table(movies);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            
+            <div className = "row_posters">
+            {/*posters container*/}
+
+                {movies.map((movie) => (
+                    <img
+                        key={movie.id}
+                        onClick={() => handleClick(movie)}
+                        className = {`row_poster ${isLargeRow && "row_posterLarge"}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        alt={movie.name} 
+                    />
+                ))}
+            </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
+        </div>
+    );
+}
+
+export default Row;
